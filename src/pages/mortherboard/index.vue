bd733d376debed104fbdf5e383e2965e5af9eeb7
00INDEX <zhangshuolove@live.com>
2022-05-02 17:55:04 +0800

    测试一下状态栏功能

--- src/pages/mortherboard/index.vue
+++ src/pages/mortherboard/index.vue
@@ -1,12 +1,19 @@
 <template>
 	<view class="content">
-		<span>11111111111111</span>
+		<span>222222222</span>
 	</view>
 </template>
 
 <script setup>
 	// 防止挡住状态栏
 	const statusBarHeight = ref(uni.getSystemInfoSync().statusBarHeight + "px")
+	// #ifdef MP-WEIXIN
+	let navigationBar = wx.getMenuButtonBoundingClientRect()
+	console.log(navigationBar)
+	const navigationBarHeight = ref((navigationBar.height + navigationBar.top * 2 - uni.getSystemInfoSync().statusBarHeight).toString() + "px")
+	// #endif
+	
+
 </script>
 
 <style scoped>
@@ -14,6 +21,9 @@
 		background-color: #fcf9ec;
 		height: 100vh;
 		width: 100%;
-		padding-top: v-bind('statusBarHeight');
+		padding-top: v-bind(statusBarHeight);
+		/* #ifdef MP-WEIXIN */
+		padding-top: v-bind(navigationBarHeight);
+		/* #endif */
 	}
 </style>
